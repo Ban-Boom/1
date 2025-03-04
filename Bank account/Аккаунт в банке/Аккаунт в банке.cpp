67e0517f7d8c456d5211e7c5fe1d17f1d844160f
Bronislav <bronkyr59@gmail.com>
2025-03-04 19:16:04 +0300

    Error correction

---
+++ Bank account/Аккаунт в банке/Аккаунт в банке.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream> 
+#include <string>   
+
+using namespace std; 
+
+struct BankAccount {
+    int accountNumber;
+    string ownerName;
+    double balance;
+};
+
+void updateBalance(BankAccount& account, double newBalance) {
+    account.balance = newBalance;
+}
+
+int main() {
+    BankAccount account;
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите номер счёта: ";
+    cin >> account.accountNumber;
+    cin.ignore(); 
+
+    cout << "Введите имя владельца: ";
+    getline(cin, account.ownerName);
+
+    cout << "Введите баланс: ";
+    cin >> account.balance;
+
+    double newBalance;
+    cout << "Введите новый баланс: ";
+    cin >> newBalance;
+
+    updateBalance(account, newBalance);
+
+    cout << "Ваш аккаунт: " << account.ownerName << "  Номер счета: "
+        << account.accountNumber << "  Обновлённый баланс: "
+        << account.balance << endl;
+
+    return 0;
+}
+
---
+++ Withdrawal of months/Вывод месяцев/Вывод месяцев.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+enum class Month {
+    NONE = 0,
+    JANUARY = 1, FEBRUARY, MARCH, APRIL, MAY, JUNE,
+    JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+};
+
+const char* monthNames[] = {
+    "", "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+};
+
+int main() {
+    int input;
+    while (true) {
+        std::cout << "Enter the month number : ";
+        std::cin >> input;
+
+        if (input == 0) {
+            std::cout << "Goodbye" << std::endl;
+            break;
+        }
+
+        Month month = static_cast<Month>(input);
+        if (month >= Month::JANUARY && month <= Month::DECEMBER) {
+            std::cout << monthNames[input] << std::endl;
+        }
+        else {
+            std::cout << "Wrong number!" << std::endl;
+        }
+    }
+    return 0;
+}
+
+
